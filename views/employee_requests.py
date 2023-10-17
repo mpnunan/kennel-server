f1ba29db9cc9a852670fd18a8dc620e35980c0a2
Michael Nunan <michael.nunan@outlook.com>
2023-10-16 19:56:57 -0500

    GET working

---
+++ views/employee_requests.py
@@ -0,0 +1,20 @@
+EMPLOYEES = [
+    {
+        "id": 1,
+        "name": "Jenna Solis"
+    },
+    {
+      "id": 2,
+      "name": "Linus Torvalds"
+    }
+]
+
+def get_all_employees():
+    return EMPLOYEES
+
+def get_single_employee(id):
+    requested_employee = None
+    for employee in EMPLOYEES:
+        if employee["id"] == id:
+            requested_employee = employee
+    return requested_employee
